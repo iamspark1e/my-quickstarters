6b191eb3bd81396ffe85d9563305107a24d0c5ee
iamspark1e <wenbo@sparkle.im>
2024-02-21 13:56:40 +0800

    fix: add frontend example

--- templates/fullstack/vite-express-drizzle-ci-template/vite.config.js
+++ templates/fullstack/vite-express-drizzle-ci-template/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
     const isProd = process.env.NODE_ENV;
     return {
-        root: "./src",
+        root: "./web",
         publicDir: "../public",
         envDir: "../",
         build: {
